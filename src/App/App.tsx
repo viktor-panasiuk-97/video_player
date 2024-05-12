7dc9075cef9412314adc0088a13471646e87b31e
Viktor Panasiuk <viktor.panasiuk97@gmail.com>
2024-05-12 18:14:40 +0200

    first commit

---
+++ src/App/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <video src="video/Cute_Baby_Rabbits.mp4" controls/>
+    </div>
+  );
+}
+
+export default App;
